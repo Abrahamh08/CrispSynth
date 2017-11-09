19a2451280327f45077ab83df4aabc2965f791ef
Cilan <abraham.hamidi01@gmail.com>
2017-11-09 17:12:26 -0600

    Update GameObject

--- include/crispsynth/game-objects/GameObject.h
+++ include/crispsynth/game-objects/GameObject.h
@@ -13,13 +13,14 @@
 class GameObject {
 protected:
     std::vector<std::shared_ptr<Component>> components;
-    float _x, _y, _sX = 1, _sY = 1, width, height;
+    float _x, _y, _z, _sX = 1, _sY = 1, _sZ = 1, width, height;
     virtual void update() {};
 public:
-    float &x = _x, &y = _y, &w = width, &h = height, &sX = _sX, &sY = _sY;
+    float &x = _x, &y = _y, &z = _z, &w = width, &h = height, &sX = _sX, &sY = _sY, &sZ = _sZ;
     virtual void update(Game &game, sf::Event& event) = 0;
     virtual void render(Game &game, float delta) {};
-    virtual void updatePosition(float tx, float ty) = 0;
+    virtual void updatePosition(float tx, float ty) {};
+    virtual void updatePosition(float tx, float ty, float tz) {};
 };
 
 #endif
